c0ad8b48eab19eb1516757f02a0979e6c0d04246
Steven Chang <1632460272@qq.com>
2019-10-11 21:08:26 +0100

    home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,17 @@
   </div>
 </template>
 
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HomePage
+  }
+}
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
