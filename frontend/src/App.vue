b5edae1c84be29c3ceed32fdaec0200279759446
Steven Chang <1632460272@qq.com>
2019-10-11 22:17:53 +0100

    navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <el-header>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1"><router-link to="/">HOME</router-link></el-menu-item>
+  <el-menu-item index="2"><router-link to="/about">About</router-link></el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">EN</template>
+    <el-menu-item index="2-1">简体中文</el-menu-item>
+    <el-menu-item index="2-2">Français</el-menu-item>
+    <el-menu-item index="2-3">Español</el-menu-item>
+    <el-menu-item index="2-4">日本語</el-menu-item>
+    <el-menu-item index="2-5">한국어</el-menu-item>
+  </el-submenu>
+   <el-submenu index="3">
+    <template slot="title">EUR</template>
+    <el-menu-item index="3-1">CNY</el-menu-item>
+    <el-menu-item index="3-2">USD</el-menu-item>
+    <el-menu-item index="3-3">CAD</el-menu-item>
+    <el-menu-item index="3-4">GBP</el-menu-item>
+    <el-menu-item index="3-5">HKD</el-menu-item>
+  </el-submenu>
+</el-menu>
+<div class="line"></div>
+    </el-header>
     <router-view/>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+import HomePage from './views/HomePage.vue'
 
 export default {
   name: 'app',
@@ -26,6 +45,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
