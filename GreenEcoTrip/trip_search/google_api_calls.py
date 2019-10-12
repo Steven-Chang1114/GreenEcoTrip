4dad96dd81d4a11044e4de27c2a6b116ae2868af
NaNkeen <ck210400@protonmail.ch>
2019-10-12 11:28:13 +0100

    Added sum of distance

--- GreenEcoTrip/trip_search/google_api_calls.py
+++ GreenEcoTrip/trip_search/google_api_calls.py
@@ -1,6 +1,7 @@
 '''
 Google API calls exposes a class that wraps common use functions for the router
 '''
+from functools import reduce
 import googlemaps
 
 class GMapsWrapper:
@@ -26,8 +27,35 @@ class GMapsWrapper:
         '''
         transit_routes_between gets the alternative transport options between start and terminal
         '''
-        self.client.directions(
+        return map(Directions, self.client.directions(
             start,
             terminal,
             mode='transit'
-        )
+        ))
+
+class Directions:
+    '''
+    Contains methods for parsing GMaps directions response
+    '''
+    def __init__(self, directions):
+        self.directions = directions
+
+    def calculate_carbon_footprint(self):
+        '''
+        Sums the carbon footprint of transit steps
+        '''
+        return self.directions
+
+    def distance(self):
+        '''
+        Returns the distance of the entire route
+        '''
+        return reduce(lambda acc, x: acc + x['distance']['value'], self.directions['legs'], 0)
+
+'''
+if __name__ == '__main__':
+    import json
+    a = GMapsWrapper('AIzaSyCUPvUnI4COqOfF73iRo32tRd8wQp_M4f8')
+    s = a.transit_routes_between('Princes Street', 'London')
+    print(json.dumps(s))
+'''
