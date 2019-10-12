bf31c9d965e90afd1006d2e6bcd84eefeb648a4b
Steven Chang <1632460272@qq.com>
2019-10-12 15:39:10 +0100

    finishing off the filter

--- frontend/src/views/Result.vue
+++ frontend/src/views/Result.vue
@@ -1,20 +1,43 @@
 <template>
 <div>
-  <el-row :gutter="3" >
-    <el-col :span="1">
-      <Drawer />
-    </el-col>
-    <el-col :span="15">
-       <F />
-      <h2 id="info">From: {{this.$store.state.departureLocation}} to {{this.$store.state.destination}}</h2>
-    </el-col>
-    <el-col :span="1" >
-        <Date id = "departure"/>
-    </el-col>
-  </el-row>
-  <Timeline id = "time"/>
- 
-  <SwitchPage id = "page"/>
+  <el-container style="height: 760px; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-menu :default-openeds="['1', '3']">
+      <el-submenu index="2">
+        <template slot="title"><i class="el-icon-menu"></i>Filter</template>
+      </el-submenu>
+      <F />
+    </el-menu>
+  </el-aside>
+  
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px;">
+      <el-row :gutter="10">
+        <el-col :span="12">
+          <h2 style="float: left; margin-top:0px; color:white">
+            Select departure trip to {{this.$store.state.destination}}
+          </h2>
+        </el-col>
+        <el-col :span="12">
+          <Date/>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+    <el-card class="box-card" width="100%">
+      <div slot="header" class="clearfix">
+        <span>Trip</span>
+        <el-button style="float: right; padding: 3px 0" type="text">Book</el-button>
+      </div>
+      <el-steps :space="200" :active="1" v-for="trip in trips" :key="trip.id" simple>
+        <el-step v-for="step in trip.steps" :key="step.id" title="Step 1" icon="el-icon-s-promotion">{{step.start}}</el-step>
+
+      </el-steps>
+    </el-card>
+    
+  </el-main>
+  </el-container>
+</el-container>
 </div>
 </template>
 
@@ -31,25 +54,67 @@ import F from './components/F'
       SwitchPage,
       Timeline,
       F
+    },
+    data() {
+      return {
+        tableData: Array(20).fill(item),
+        trips: [
+        {
+          id: 0,
+          steps: [
+          {id: 0, start: "Barcelona", end: "London", type: "flight"},
+          {id: 1, start: "London", end: "Edinburgh", type: "rail"}
+        ]},
+        ]
+      }
     }
   };
 </script>
 
 <style>
-#info{
-  text-align: left;
-  padding-left: 500px;
-}
-#departure{
-  margin-top: 16px;
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
   
-}
-#page{
-  text-align: center;
-  margin-top: 500px;
-}
-#time{
-  padding-left: 560px;
-  padding-top: 10px;
-}
+  .el-aside {
+    color: #333;
+  }
+
+  #departure{
+    padding-right: 500px;
+  }
+
+  #line{
+    padding-right: 20px;
+    padding-bottom: 20px;
+  }
+
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .box-card{
+    padding-left: 50px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    margin-left:50px;
+    margin-right:50px;
+    margin-top: 30px;
+  }
 </style>
--- frontend/src/views/components/F.vue
+++ frontend/src/views/components/F.vue
@@ -1,12 +1,41 @@
 <template>
 <div>
-  <h2>Filter</h2>
+  <div>
+    <p style = "margin-left: 15px;">Sorted by</p>
+    <el-select id = "sort" size = small v-model="value" placeholder="Select" style = "margin-left:15px; margin-right:20px;">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+  </div>
+  <el-divider></el-divider>
   <div class="block">
-    <el-slider
-      v-model="value"
+    <p style = "margin-left: 15px;">The departure time</p>
+     <el-slider
+      v-model="time"
       range
-      :marks="marks">
-    </el-slider>
+      :marks="marks"
+      :max = 24
+      style = "margin-left:20px; margin-right: 20px"
+      >
+    </el-slider><br>
+  </div>
+
+    <el-divider></el-divider>
+
+    <div class="block">
+    <p style = "margin-left: 15px;">The price</p>
+     <el-slider
+      v-model="price"
+      range
+      :marks="mark"
+      :max = 2000
+      style = "margin-left:20px; margin-right: 20px"
+      >
+    </el-slider><br>
   </div>
   </div>
 </template>
@@ -15,19 +44,46 @@
   export default {
     data() {
       return {
-        value: [30, 60],
-        marks: {
-          0: '0°C',
-          8: '8°C',
-          37: '37°C',
-          50: {
+        price: [400, 1000],
+        mark: {
+          0:'0$',
+          1000: {
             style: {
               color: '#1989FA'
             },
-            label: this.$createElement('strong', '50%')
-          }
-        }
+            label: this.$createElement('strong', '1000$')
+          },
+          2000:'2000$'
+        },
+        time: [6, 18],
+        marks: {
+          0: '0am',
+          6: '6am',
+          12: {
+            style: {
+              color: '#1989FA'
+            },
+            label: this.$createElement('strong', 'noon')
+          },
+          18: '18pm',
+          24:'24pm'
+        },
+        options: [{
+          value: 'Greenest',
+          label: 'Greenest'
+        }, {
+          value: 'Fastest',
+          label: 'Fastest'
+        }, {
+          value: 'Cheapest',
+          label: 'Cheapest'
+        }],
+        value: ''
       }
     }
   }
 </script>
+
+<style scoped>
+
+</style>
